f9d80b8e39e8b1e8623a3a34e01425c2380e3fed
Nanjesh Ramesh <nanjeshramesh7@gmail.com>
2020-04-09 11:55:14 -0700

    leetcode solution

---
+++ oddevenlist.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = head;
+        ListNode even = head.next;
+        ListNode dummy = even;
+        while (even != null && even.next != null) {
+            cur.next = cur.next.next;
+            even.next = even.next.next;
+            cur = cur.next;
+            even = even.next;
+        }
+        cur.next = dummy;
+        return head;
+    }
+}
