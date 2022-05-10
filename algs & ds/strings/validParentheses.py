0ae2f80b1511caf2e948f50e8610909de917f7ef
Nanjesh <nanjeshramesh7@gmail.com>
2022-05-10 11:21:52 -0700

    Update validParentheses.py

--- algs & ds/strings/validParentheses.py
+++ algs & ds/strings/validParentheses.py
@@ -1,1 +1,25 @@
+#validParentheses
+
+def isValid(self, s:str) -> bool:
+	if len(s) % 2 != 0:
+		return False
+
+	dic = {'(':')','[':']','{':'}'}
+	stack = []
+
+	a = dic.keys()
+
+	for i in s:
+		if i in dic:
+			stack.append(i)
+		else:
+			if stack == []:
+				return False
+			
+			b = stack.pop()
+
+			if i != dic(b):
+				return False
+
+	return stack[]
 
