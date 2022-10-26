f8806b2a50d0a806f89594d9e3fba9b90a8dcab4
Nanjesh <nanjeshramesh7@gmail.com>
2022-10-25 23:51:45 -0700

    Update patientsWithCondition.sql

--- database/patientsWithCondition.sql
+++ database/patientsWithCondition.sql
@@ -1,3 +1,10 @@
+SELECT * FROM PATIENTS
+WHERE conditions regexp '^DIAB1| DIAB1';
+
+/*
+passes 13/14 test-cases
 select patient_id, patient_name, conditions
 from Patients
 where conditions LIKE '%DIAB1%';
+*/
+
